4464262e87f073dcbe4c52e98cd922cb7732125e
MoniqueDaw <ginbloess@gmail.com>
2022-11-09 23:49:55 -0500

    added search position on load

--- script.js
+++ script.js
@@ -76,3 +76,5 @@ function handleSubmit(event) {
 
 let form = document.querySelector("search-form");
 form.addEventListener("submit", handleSubmit);
+
+search(navigator.geolocation.getCurrentPosition(showPosition));
