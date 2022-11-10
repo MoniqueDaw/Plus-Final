367703cac834c12cb0b0d930874f1f168633981e
MoniqueDaw <ginbloess@gmail.com>
2022-11-09 20:33:14 -0500

    updated JS time

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
-let now = new Date();
+function formatDate(date) {
+  let date = new Date(date);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
-let currentDate = document.querySelector("#currentDate");
-
-let date = now.getDate();
-let hours = now.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDate()];
+  return `${day} ${hours}:${minutes}`;
 }
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-let h2 = document.querySelector("h2");
-h2.innerHTML = `${day} ${date} ${hours}:${minutes}`;
 
 function displayWeatherCondition(response) {
   let temp = Math.round(response.data.main.temp);
@@ -36,6 +34,8 @@ function displayWeatherCondition(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
   let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = response.data.main.humidity;
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(currentTime);
 }
 
 function showPosition(position) {
@@ -65,11 +65,9 @@ function searchCity(event) {
 
 function search(city) {
   let apiKey = "72bb9dab46b9ec3d65f423c63f27a9b8";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
 
 let form = document.querySelector("form");
 form.addEventListener("submit", searchCity);
-
-search("Paris");
